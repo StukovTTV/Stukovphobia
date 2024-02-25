150d3eabe5117860d2e4f8956f207ba29e881f8d
StukovTTV <spam@robertstraif.net>
2024-02-25 10:05:00 +0100

    Cooperations Reworked
    Ground Weapons available change due to cooperation
    Researches combined into one

--- scripts/_basebuilt.lua
+++ scripts/_basebuilt.lua
@@ -17,8 +17,6 @@ UnlockKnowledge( "weapon.rifle" );
 UnlockKnowledge( "weapon.sniper" );
 UnlockKnowledge( "weapon.machinegun" );
 UnlockKnowledge( "weapon.rocketlauncher" );
-UnlockKnowledge( "weapon.farmershotgun" );
-UnlockKnowledge( "weapon.ak47" );
 
 
 UnlockKnowledge( "armour.basic" );
@@ -36,42 +34,34 @@ UnlockKnowledge( "DefenceBattery" );
 
 UnlockResearch( "Researches.AlienInvasion" );
 
-UnlockManufacture( "ManTech.FragGrenade" );
-UnlockManufacture( "ManTech.FragRocket" );
-UnlockManufacture( "ManTech.FlashGrenade" );
-UnlockManufacture( "ManTech.SmokeGrenade" );
-UnlockManufacture( "ManTech.C4" );
+--UnlockItem( "weapon.grenade.medipack" );
+--UnlockItem( "weapon.combatshield" );
 
-UnlockItem( "weapon.grenade.medipack" );
-UnlockItem( "weapon.combatshield" );
+--UnlockItem( "weapon.rocket.frag" );
+--UnlockItem( "weapon.grenade.frag" );
+--UnlockItem( "weapon.grenade.smoke" );
+--UnlockItem( "weapon.grenade.flashbang" );
+--UnlockItem( "weapon.explosive.C4" );
 
-UnlockItem( "weapon.rocket.frag" );
-UnlockItem( "weapon.grenade.frag" );
-UnlockItem( "weapon.grenade.smoke" );
-UnlockItem( "weapon.grenade.flashbang" );
-UnlockItem( "weapon.explosive.C4" );
-
-UnlockItem( "weapon.farmershotgun" );
+--UnlockItem( "weapon.farmershotgun" );
+--UnlockItem( "weapon.ak47" );
+--UnlockItem( "weapon.pistol" );
+--UnlockItem( "weapon.rifle" );
+--UnlockItem( "weapon.sniper" );
+--UnlockItem( "weapon.machinegun" );
+--UnlockItem( "weapon.rocketlauncher" );
+--UnlockItem( "weapon.farmershotgun" );
+--UnlockItem( "weapon.ak47" );
 --UnlockItem( "weapon.shotgun" );
-UnlockItem( "weapon.pistol" );
-UnlockItem( "weapon.rifle" );
-UnlockItem( "weapon.sniper" );
-UnlockItem( "weapon.machinegun" );
-UnlockItem( "weapon.rocketlauncher" );
 
 UnlockItem( "AV.SIDEWINDER" );
 UnlockItem( "AV.AUTOCANNON" );
 UnlockItem( "AV.AVALANCHE" );
 
-StockItemForFirstBase( "weapon.grenade.frag", 20 );
-StockItemForFirstBase( "weapon.grenade.smoke", 20 );
-StockItemForFirstBase( "weapon.grenade.flashbang", 20 );
-StockItemForFirstBase( "weapon.rocket.frag", 10 );
-StockItemForFirstBase( "weapon.explosive.C4", 10 );
-StockItemForFirstBase( "weapon.grenade.stimpack", 10 );
-StockItemForFirstBase( "weapon.grenade.painkillers", 10 );
-
-
+--StockItemForFirstBase( "weapon.farmershotgun", 20 );
+--StockItemForFirstBase( "weapon.ak47", 20 );
+--UnlockManufacture( "ManTech.farmershotgun" );
+--UnlockManufacture( "ManTech.ak47" );
 --BuildAlienBase( 3853, 1600 );
 --UnlockFinalMission();
 
